cda8df2bbb1d5daf7da74ce005738c20b5157818
Piyush Khatri <piyushkhatri968@gmail.com>
2025-04-14 03:15:13 -0700

    a

--- client/config.js
+++ client/config.js
@@ -1,1 +1,3 @@
-export const Frontend_URL = "http://localhost:4000/api/v1";
+// export const Frontend_URL = "http://localhost:4000/api/v1";
+export const Frontend_URL =
+  "https://library-management-system-backend-pied.vercel.app/api/v1";
