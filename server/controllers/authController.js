09212d463bd5392b9fee9e9159b3922a55e6c084
Piyush Khatri <piyushkhatri968@gmail.com>
2025-04-14 03:29:50 -0700

    .

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -133,18 +133,22 @@ export const login = catchAsyncErrors(async (req, res, next) => {
 });
 
 export const logout = catchAsyncErrors(async (req, res, next) => {
-  res
-    .status(200)
-    .cookie("token", "", {
-      expires: new Date(0), // or use: maxAge: 0
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-    })
-    .json({
-      success: true,
-      message: "Logged out successfully.",
-    });
+  try {
+    res
+      .status(200)
+      .cookie("token", "", {
+        expires: new Date(0),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // must match
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // must match
+      })
+      .json({
+        success: true,
+        message: "Logged out successfully.",
+      });
+  } catch (error) {
+    return next(new ErrorHandler(error), 404);
+  }
 });
 
 export const getUser = catchAsyncErrors(async (req, res, next) => {
