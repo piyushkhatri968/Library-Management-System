69aabe64f6593983b32636e9d793d6c7052d89bb
Piyush Khatri <piyushkhatri968@gmail.com>
2025-04-14 03:05:58 -0700

    .

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -7,8 +7,8 @@ export const sendToken = (user, statusCode, message, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      secure: true,
-      sameSite: "None",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     })
     .json({
       success: true,
